c8bc16cd27c259cdb2fe07657de603b72b72b567
Vignesh Mani <vigneshms565@gmail.com>
2024-12-06 21:56:58 +0800

    update: black-scholes dividends
    - dividend incorporated in estimation
    - modularized helper functions
    - fixed date to expiry
    - added function definitions

--- black-scholes/blackScholesModel.py
+++ black-scholes/blackScholesModel.py
@@ -2,21 +2,57 @@ import numpy as np
 import scipy.stats as si
 
 class BlackScholesModel:
-    def __init__(self, S, K, T, r, sigma):
-        self.S = S        # Underlying asset price
-        self.K = K        # Option strike price
-        self.T = T        # Time to expiration in years
-        self.r = r        # Risk-free interest rate
-        self.sigma = sigma  # Volatility of the underlying asset
+    '''
+    Class to calculate the price of a call or put option using the Black-Scholes model.
+    '''
+    def __init__(self, S, K, T, r, sigma, q):
+        '''
+        Args:
+        - S (float): The spot price of the underlying stock.
+        - K (float): The strike price of the option.
+        - T (float): The time to maturity of the option.
+        - r (float): The risk-free rate.
+        - sigma (float): The volatility of the underlying stock.
+        - q (float): The dividend yield of the underlying stock.
+        '''
+        self.S = S 
+        self.K = K 
+        self.T = T 
+        self.r = r 
+        self.sigma = sigma
+        self.q = q
+
+    @property
+    def params(self):
+        return {'S': self.S, 
+                'K': self.K, 
+                'T': self.T, 
+                'r':self.r,
+                'q':self.q,
+                'sigma':self.sigma}
 
     def d1(self):
-        return (np.log(self.S / self.K) + (self.r + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * np.sqrt(self.T))
+        '''
+        Calculates the d1 parameter used in the Black-Scholes formula.
+        '''
+        return (np.log(self.S / self.K) + (self.r -self.q + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * np.sqrt(self.T))
     
     def d2(self):
+        '''
+        Calculates the d2 parameter used in the Black-Scholes formula.
+        '''
         return self.d1() - self.sigma * np.sqrt(self.T)
     
-    def call_option_price(self):
-        return (self.S * si.norm.cdf(self.d1(), 0.0, 1.0) - self.K * np.exp(-self.r * self.T) * si.norm.cdf(self.d2(), 0.0, 1.0))
+    def call_option_price(self) -> float:
+        '''
+        Calculates the price of a call option using the Black-Scholes formula.
+        Returns: call_option_price (float): The price of the call option.
+        '''
+        return (self.S*np.exp(-self.q*self.T) * si.norm.cdf(self.d1(), 0.0, 1.0) - self.K * np.exp(-self.r * self.T) * si.norm.cdf(self.d2(), 0.0, 1.0)).item()
     
-    def put_option_price(self):
-        return (self.K * np.exp(-self.r * self.T) * si.norm.cdf(-self.d2(), 0.0, 1.0) - self.S * si.norm.cdf(-self.d1(), 0.0, 1.0))
+    def put_option_price(self) -> float:
+        '''
+        Calculates the price of a put option using the Black-Scholes formula.
+        Returns: put_option_price (float): The price of the put option.
+        '''
+        return (self.K * np.exp(-self.r * self.T) * si.norm.cdf(-self.d2(), 0.0, 1.0) - self.S*np.exp(-self.q*self.T) * si.norm.cdf(-self.d1(), 0.0, 1.0)).item()
---
+++ black-scholes/utilsBlackScholes.py
@@ -0,0 +1,51 @@
+import numpy as np
+import yfinance as yf
+
+def fetch_stock_data(ticker_symbol: str):
+    '''
+    Fetches stock data for a given ticker symbol.
+    Args:   ticker_symbol (str): The ticker symbol of the stock.
+    Returns: stock_data (pd.DataFrame): The stock data.
+    '''
+    stock_data = yf.download(ticker_symbol, period='1y', interval='1d')
+    return stock_data
+
+def fetch_options_data(ticker_symbol: str):
+    '''
+    Fetches options data for a given ticker symbol.
+    Args:   ticker_symbol (str): The ticker symbol of the stock.
+    Returns: options_data (tuple): A tuple containing the expiry date, call options data, and put options data.
+    '''
+    ticker = yf.Ticker(ticker_symbol)
+    options_dates = ticker.options
+    options_data = ticker.option_chain(options_dates[5])  # Closest expiry date [Expand to include more]
+    return options_dates[5], options_data.calls, options_data.puts
+
+def fetch_risk_free_rate():
+    '''
+    Fetches the risk-free rate.
+    Returns: risk_free_rate (float): The risk-free rate based on the 13-week Treasury Bill.
+    '''
+    rfr = yf.download("^IRX", period='1d')
+    return rfr['Close'].iloc[-1]
+
+def fetch_dividend_yield(ticker_symbol: str) -> float:
+    '''
+    Fetches the dividend yield for a given stock.
+    Args:   ticker_symbol (str): The ticker symbol of the stock.
+    Returns: dividend_yield (float): The dividend yield.
+    '''
+    dividend_yield = float(yf.Ticker(ticker_symbol).info['dividendRate']/ yf.Ticker(ticker_symbol).info['previousClose'])
+    return dividend_yield
+
+def calculate_historical_volatility(stock_data, window=252) -> float:
+    '''
+    Calculates the historical volatility of a stock.
+    Args:
+    - stock_data (pd.DataFrame): The stock data.
+    - window (int): The window size for calculating the historical volatility.
+    Returns: volatility (float): The historical volatility of the stock.
+    '''
+    log_returns = np.log(stock_data['Close'] / stock_data['Close'].shift(1))
+    volatility = np.sqrt(window) * log_returns.std()
+    return volatility
